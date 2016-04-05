a2380a1c413ac707f4938eb6324389e9f557b196
ngocquyhoang <ngocquyhoang.93@gmail.com>
2016-04-05 19:21:32 +0700

    upload code

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+	<meta property="og:locale" content="vi_VN" />
+	<meta property="og:description" content="" />
+	<meta property="og:site_name" content="" />
+	<meta property="og:title" content=" " />
+	<meta property="og:url" content="http://up.ssc.vn/images/214818images1066169_ngu_y.jpg" />
+	<meta property="og:image" content="http://up.ssc.vn/images/214818images1066169_ngu_y.jpg" />
+</head>
+<body>
+	
+</body>
+</html>
